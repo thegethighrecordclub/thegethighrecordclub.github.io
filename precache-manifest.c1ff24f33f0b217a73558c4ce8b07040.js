36730e8973b50a2b9a0c600a7d9dfff9d5f340a6
Adrienne Kerr <akerr@lmtc-adkerr.jomax.paholdings.com>
2020-01-20 22:39:50 -0800

    neon polygon kaleidos website with about page

---
+++ precache-manifest.c1ff24f33f0b217a73558c4ce8b07040.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b9490cf3f220bbc363276903b5faaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bdfd1dcd70872130894",
+    "url": "/static/css/main.100a634d.chunk.css"
+  },
+  {
+    "revision": "28fff72065f8f92c6acb",
+    "url": "/static/js/2.bee34798.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.bee34798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bdfd1dcd70872130894",
+    "url": "/static/js/main.d1e04caf.chunk.js"
+  },
+  {
+    "revision": "e4469507823179d012e8",
+    "url": "/static/js/runtime-main.eba7fc2a.js"
+  },
+  {
+    "revision": "173eab63865cda5a0509275aff474f6c",
+    "url": "/static/media/mandala.173eab63.jpg"
+  }
+]);
